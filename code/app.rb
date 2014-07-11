43d09da2671cf0e289325824ce075eba29b252e3
macosgrove <maryannecos@gmail.com>
2014-07-11 21:23:40 +1000

    Serve dummy site data

--- code/app.rb
+++ code/app.rb
@@ -17,6 +17,15 @@ class App < Sinatra::Application
     markdown readme, fenced_code_blocks: true, autolink: true
   end
 
+  get '/acorn-sat/v1/sites' do
+    '[
+        {"id": "069018", "site": "Moruya Heads", "lat": -35.909, "long": 150.153 },
+        {"id": "070351", "site": "Canberra", "lat": -35.309, "long": 149.2 },
+        {"id": "072150", "site": "Wagga Wagga", "lat": -35.158, "long": 147.457 },
+        {"id": "072161", "site": "Cabramurra", "lat": -35.937, "long": 148.378 }
+    ]'
+  end
+
   get '/examples' do
     content_type :json
     ExampleModel.all.to_json
