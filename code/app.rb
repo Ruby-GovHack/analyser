ee2860c729590babb61ee14478742e69aa63cdda
Hugh Evans <hugh@hughevans.net>
2014-07-11 22:27:12 +1000

    Add Access-Control-Allow-Origin *

--- code/app.rb
+++ code/app.rb
@@ -18,6 +18,8 @@ class App < Sinatra::Application
   end
 
   get '/acorn-sat/v1/sites' do
+    cors_headers
+
     '[
         {"id": "069018", "site": "Moruya Heads", "lat": -35.909, "long": 150.153 },
         {"id": "070351", "site": "Canberra", "lat": -35.309, "long": 149.2 },
@@ -27,6 +29,8 @@ class App < Sinatra::Application
   end
 
   get '/monthly/acorn-sat/v1/max-temp' do
+    cors_headers
+
     '[
         {"id": "069018", "max":20.02, "std-dev": 5.11 },
         {"id": "070351", "max":18.48, "std-dev": 3.95 },
@@ -38,6 +42,14 @@ class App < Sinatra::Application
   get '/examples' do
     content_type :json
     ExampleModel.all.to_json
+  end
+
+  # FIXME replace with rack-cors
+  def cors_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 
 end
