7a468aa495b8794c7fb20b68382fb1b801c0227a
macosgrove <maryannecos@gmail.com>
2014-07-12 19:20:44 +1000

    Provide cut down data for tests, full data for prod and dev

--- code/db/seeds.rb
+++ code/db/seeds.rb
@@ -4,4 +4,4 @@ require_relative '../models/data_analyser'
 
 provider = SparqlDataProvider.new('acorn-sat')
 Site.fetch(provider)
-DataAnalyser.fetch(provider, "01-1935", "01-1945", 1-35.0, 150.5, -36.0, 147.2)
+DataAnalyser.fetch(provider)
--- code/db/test_seeds.rb
+++ code/db/test_seeds.rb
@@ -4,4 +4,5 @@ require_relative '../models/data_analyser'
 
 provider = SparqlDataProvider.new('acorn-sat')
 Site.fetch(provider)
-#DataAnalyser.fetch(provider)
+DataAnalyser.fetch(provider, "01-2000", "01-2011", 1-35.0, 150.5, -36.0, 147.2)
+# DataAnalyser.fetch(provider)
