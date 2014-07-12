98e979862b3b00f795a2cb093f26f5c6bbcc0b12
Tim Cosgrove <tim.rccosgrove@gmail.com>
2014-07-12 23:07:33 +1000

    restructured return format for MonthlyData
    soon to support selecting parameters

--- code/spec/monthly_data_spec.rb
+++ code/spec/monthly_data_spec.rb
@@ -8,6 +8,6 @@ describe MonthlyData do
   it 'should fetch data from mongoid' do
     site = Site.all_as_hash['072161']
     data = MonthlyData.filter_all(site, '08-2008', '08-2008').to_json
-    expect(data).to eq('[{"month":"08-2008","high_max_temp":8.2}]')
+    expect(data).to include('"month":"08-2008","high_max_temp":8.2')
   end
 end
