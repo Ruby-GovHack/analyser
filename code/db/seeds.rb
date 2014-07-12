e42159c2b3faa0a132b87e798899af388dec0105
macosgrove <maryannecos@gmail.com>
2014-07-12 16:07:26 +1000

    Rake seed

--- code/db/seeds.rb
+++ code/db/seeds.rb
@@ -1,5 +1,5 @@
 require_relative '../models/site'
 require_relative '../models/data_provider'
 
-provider = DataProvider.new(provider_details(params[:dataset]))
-Site.fetch(provider).to_json
+provider = DataProvider.new('acorn-sat')
+Site.fetch(provider)
