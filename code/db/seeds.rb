48d2e9733e20d3f25541db2abcbca31498e1e797
macosgrove <maryannecos@gmail.com>
2014-07-12 16:14:40 +1000

    Hook in DataAnalyser

--- code/db/seeds.rb
+++ code/db/seeds.rb
@@ -1,5 +1,7 @@
 require_relative '../models/site'
 require_relative '../models/data_provider'
+require_relative '../models/data_analyser'
 
 provider = DataProvider.new('acorn-sat')
 Site.fetch(provider)
+DataAnalyser.fetch(provider)
---
+++ code/models/data_analyser.rb
@@ -0,0 +1,5 @@
+class DataAnalyser
+  def self.fetch(provider)
+   puts "Replace this with the data analysis stuff"
+  end
+end
