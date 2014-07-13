0143868ec825f77d315ead2891acc0a2981e116e
Tim Cosgrove <tim.rccosgrove@gmail.com>
2014-07-13 15:07:08 +1000

    Data restored!

--- code/spec/spec_helper.rb
+++ code/spec/spec_helper.rb
@@ -12,11 +12,11 @@ RSpec.configure do |conf|
   conf.include Rack::Test::Methods
 
   conf.before(:suite) do
-    Mongoid.load!("./config/mongoid.yml", :developer)
+    Mongoid.load!("./config/mongoid.yml", :test)
 
-    #DatabaseCleaner[:mongoid].strategy = :truncation
-    #DatabaseCleaner[:mongoid].clean_with(:truncation)
-    #Seeds.new.seed_test
+    DatabaseCleaner[:mongoid].strategy = :truncation
+    DatabaseCleaner[:mongoid].clean_with(:truncation)
+    Seeds.new.seed_test
   end
 
  end
